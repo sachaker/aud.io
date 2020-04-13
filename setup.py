d7b19f53a6c94be5c338cab0a0097463ef51a553
sachaker <midgetman69@gmail.com>
2020-04-12 23:55:50 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aud.io',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Sacha McElligott',
+    author_email='sacha@nyu.edu',
+    description=''
+)
